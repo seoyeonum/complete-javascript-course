be4a16a5c575f2c96c415958a6152a40bb8242b6
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-07-22 16:58:48 +0900

    📝 chore: add examples from JS fundamentals pt1
    - practiced values, variables, and data types
    - followed along with instructor examples

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,66 @@
+/*
+let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+console.log("Seoyeon");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name convention
+
+// variable names cannot start with numbers
+// let 3years = 3;    // Uncaught SyntaxError: Invalid or unexpected token
+
+// variable names cannot contain '&'
+// let seoyeon&matilda = 'JM';    // Uncaught SyntaxError: Unexpected token '&'
+let seoyeon_matilda = "JM";
+
+// 'new', 'function', ... are reserved keyword
+// let new = 27;    // Uncaught SyntaxError: Unexpected token 'new'
+// let function = 27;    // Uncaught SyntaxError: Unexpected token 'function'
+let sfunction = 27;
+
+// name 키워드의 경우 사용 가능하지만 권장하지 않음
+// → firstName 의 식으로 사용하기를 권장함
+// name = 'Seoyeon';
+
+// not illegal but do not recommend using variable names start with upper character
+// let Person = 'seoyeon';
+let person = "seoyeon";
+
+// For numbers that will never change, capitalize the variable name
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "James");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
