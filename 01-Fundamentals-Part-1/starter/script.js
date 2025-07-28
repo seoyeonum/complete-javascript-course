555b0f244cb132f03eab819e537b9fad5162ecce
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-07-28 23:41:15 +0900

    📝 chore: add examples from JS fundamentals pt1
    - practiced control structure (if/esle statements)
    - followed along with instructor examples

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -132,7 +132,7 @@ console.log(x, y);
 
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
-*/
+
 
 const firstName = "Jonas";
 const job = "teacher";
@@ -159,3 +159,28 @@ lines`);
 console.log(`String
 multiple
 lines`);
+*/
+
+// control structure → if / else statements
+
+const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+  // let century = 20;
+  century = 20;
+} else {
+  // let century = 21;
+  century = 21;
+}
+// console.log(century);    //→ Uncaught ReferenceError: century is not defined
+console.log(century);
