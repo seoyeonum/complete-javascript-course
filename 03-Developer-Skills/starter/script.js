a1c1053b0c7d11362dc3b7253cfd630b1f383510
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-08-11 21:57:11 +0900

    📝comment: add advice for writing code
    - added comments for learning how to code
    - added comments how to think as developer

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*
 // { "singleQuote": true, "arrowParens": "avoid" } 의 형태로 저장된 .prettierrc
 // 이 외에도 format에 있어 필요한 부분을 JSON 파일로 정의해둘 수 있다.
 // ※ JSON 파일은 주석을 허용하지 않는다.
@@ -29,3 +30,30 @@ const calcAge = birthYear => 2037 - birthYear;
 // : terminal에 live-server 입력
 
 console.log('Success installing node.js and live-server');
+*/
+
+// learing how to code
+
+// 1. 구체적인 목표 설정하기 ex. 1년 이내 웹 개발자가 될 것이다.
+// 2. 배우는 코드를 이해하고 작성하기
+// 3. 새로운 기능이나 개념을 배운 뒤 바로 활용하기 (지식 강화) ex. 메모, challenge 등
+// 4. 강의 외 환경에서도 자기 만의 코드 작성하기 (프로젝트)
+// 5. 처음부터 완벽한 코드를 작성하기 위해 노력하지 말기
+// 6. 한 번에 너무 많은 것을 배우기 위해 애쓰지 말기 (의욕 잃지 않기)
+// 7. 혼자 공부하지 않기 (온라인, 오프라인, ...) ex. SNS에 #100DaysOfCode 등 다른 사람에게 설명하거나 공유하기
+// 8. 강의 과정을 들었다고 끝이라고 생각하지 말기 (시작일 뿐이다.)
+
+// ※ 겁먹지 말고 미리 준비하고 성공하자 🔥
+
+// how to think as developer : become a problem solver
+
+// 1. 차분하고 천천히. 계획을 건너뛰지 말기
+// 2. 논리적이고 이성적인 접근하기
+// 3. 4-step framework 를 사용하기
+//    1) 문제를 100% 이해하기 (Big Picture)
+//    2) 분할하고 정복하기. 하위 문제부터 차근히 해결하기
+//    3) 검색을 두려워하거나 부끄러워하지 말기. (google, stackoverflow, mdn, ...)
+//    4) 실제 코드 작성 전에 의사코드(pseudo-code) 작성하기
+//      : 규칙은 없다. 이해할 수 있는 수준이면 된다.
+
+// + 세상 일에 대해 호기심과 자신감을 키우자.
