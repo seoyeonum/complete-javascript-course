18476b080213358e1f6db54ef5cae3dd2a3acd6b
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-08-15 23:49:12 +0900

    📝 comment: add comments and examples about DOM
    - added comments about DOM tree structure
    - practiced using DOM methods (querySelector and textContent)
    - followed along with instructor examples

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// DOM & DOM Manupulation
+// querySelector(), textContent
+console.log(document.querySelector('.message').textContent);
+
+// DOM (Document Object Model)
+// HTML 과 JavaScript 를 이어주는 역할
+// DOM Tree Structure 에서 Documet는 DOM의 가장 상위 요소이다.
+// → Document is special object that is entry point to the DOM.
+
+// DOM !== JavaScript
+// DOM 과 DOM Methods, DOM Properties는 JavaScript와 교류할 수 있는 Web API의 일부이다.
+// (Web API 에는 DOM 외에도 Timers, Fetch 등이 있다.)
