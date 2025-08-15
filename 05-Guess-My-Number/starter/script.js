f95492605b80ff8cefab35ac75ab63f00a512c29
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-08-16 00:24:41 +0900

    📝 chore: add examples about Events
    - practiced handling click events using eventListener (addEventLister)
    - followed along with instructor examples

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // DOM & DOM Manupulation
 // querySelector(), textContent
 console.log(document.querySelector('.message').textContent);
@@ -22,3 +23,21 @@ document.querySelector('.score').textContent = 10;
 // input 필드에서 값을 가져오기 위해서는 .value를 사용
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+// events is something that happens on the page.
+// with an event listener,
+// we can wait that certain events happen and then reat to it.
+
+// addEventListener 는 특수한 형태의 function이라고 보면 된다.
+// → parameter를 2개 건네 받는다.
+// 1) 'click' 이벤트 발생 시
+// 2) 다음 작업을 수행
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
