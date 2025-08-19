751f93dab571349dcceaff3c4a21f049cd738340
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-08-19 17:16:27 +0900

    🔨 chore: add examples about DOM and Events
    - create function called init
    - refactored the code
    - followed along with instructor examples

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -14,15 +14,30 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting conditions
+const init = function () {
+  // 함수 외부에서는 접근이 불가능
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  // class 에 이미 존재 한다면, 중복 추가하지 않는다.
+  // class 에 이미 없어도, 에러 발생하지 않는다.
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -70,8 +85,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2. Check if player's score is >= 100
-    // 테스트 편의상 20 으로 우승 기준 변경
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -90,28 +104,4 @@ btnHold.addEventListener('click', function () {
 
 // Challenge #2
 // Resetting the game when player click the button New
-btnNew.addEventListener('click', function () {
-  // Resetting playing status
-  playing = true;
-  activePlayer = 0;
-
-  // Resetting current score
-  currentScore = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-
-  // Resetting player score
-  scores[0] = 0;
-  scores[1] = 0;
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-
-  // Resetting styles
-  if (player0El.classList.contains('player--winner')) {
-    player0El.classList.remove('player--winner');
-  } else if (player1El.classList.contains('player--winner')) {
-    player1El.classList.remove('player--winner');
-  }
-
-  player0El.classList.add('player--active');
-});
+btnNew.addEventListener('click', init);
