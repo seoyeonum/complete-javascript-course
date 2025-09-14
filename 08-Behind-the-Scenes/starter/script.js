c9d70d50ab61ca9f6f3f4dde573c0842f220e525
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-09-14 14:59:16 +0900

    📝 comment: add comments about what is JavaScript
    - add comments about an high-level overview of JavaScript
    - followed along with instructor lectures

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+/*
+※ What is JavaScript?
+1. High-level
+2. Garbage-collected
+3. Interpreted or just-in-time compiled(JIT)
+4. Multi-paradigm
+5. Prototype-based object-oriented
+6. First-class functions
+7. Dynamic
+8. Single-threaded
+9. Non-blocking event loop
+ */
