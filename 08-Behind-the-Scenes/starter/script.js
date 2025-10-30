0057375b632649be0955f67d96023746881b78a8
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-10-30 15:44:10 +0900

    📝 chore: add examples about the scope chain
    - learn about three kinds of the scope (global, function and block)
    - practice the scope chain with writing code
    - follow along with instructor lectures

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,14 +1,39 @@
 'use strict';
 
-/*
-※ What is JavaScript?
-1. High-level
-2. Garbage-collected
-3. Interpreted or just-in-time compiled(JIT)
-4. Multi-paradigm
-5. Prototype-based object-oriented
-6. First-class functions
-7. Dynamic
-8. Single-threaded
-9. Non-blocking event loop
- */
+// Global scope
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    // Block scope
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var milenial = true; // var type variable follows Function scoped
+      // Creating NEW variable with same name as outer scopes; variable
+      const firstName = 'Steven';
+
+      // Reassigning outer scope's variable
+      output = 'NEW OUTPUT!';
+
+      const str = `Oh, and you're a milenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(milenial);
+    // console.log(add(2, 3)); // strict mode 를 해제할 경우, add 호출 가능!
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Seoyeon';
+calcAge(1991);
+// console.log(age);
+// printAge();
