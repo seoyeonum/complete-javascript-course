cc07345e5aef70f488f20297a1648b4f12da9d11
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-12-12 21:32:39 +0900

    📝 chore: add examples about forEach
    - add examples about looping arrays (forEacch)
    - compare forEach method with Array.entries()
    - follow along with instructor lectures

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -120,6 +120,7 @@ console.log(letters.join('-'));
 // → a-b-c-d-e-f-g-h-i-j
 */
 
+/*
 // ※ The New at Method
 // : ES2022부터 업데이트 된 at 메소드
 
@@ -137,3 +138,31 @@ console.log(arr.at(-1)); // → 64
 // at 메서드는 String에도 이용 가능하다.
 console.log('jonas'.at(0)); // → j
 console.log('jonas'.at(-1)); // → s
+*/
+
+// ※ Looping Arrays: forEach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---- FOREACH ----');
+movements.forEach(function (mov, i, arr) {
+  // 첫값, 인덱스, 배열
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+// →
+// Movement 1: You deposited 200
+// Movement 2: You deposited 450
+// Movement 3: You withdrew 400
+// ...
