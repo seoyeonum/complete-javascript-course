18289bdd33a73a51f5f9e4958342cf251230bace
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-01-03 16:45:34 +0900

    📝 chore: add examples about groupBy methods
    - add examples about groupBy method of Array(Object)
    - apply syntax to the callback fn of groupBy method to simplify the code
    - follow along with instructor lectures

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -13,6 +13,7 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  type: 'premium',
 };
 
 const account2 = {
@@ -20,6 +21,7 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  type: 'standard',
 };
 
 const account3 = {
@@ -27,6 +29,7 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  type: 'premium',
 };
 
 const account4 = {
@@ -34,6 +37,7 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  type: 'basic',
 };
 
 // (실제 API 처럼 느껴지도록 아래와 같이 Array 형태로 묶었다.)
@@ -640,6 +644,7 @@ const overalBalance2 = accounts
 console.log(overalBalance2); // → 17840
 */
 
+/*
 // ※ Sorting Arrays
 const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
 
@@ -675,3 +680,39 @@ console.log(movements);
 movements.sort((a, b) => b - a);
 console.log(movements);
 // → [3000, 1300, 450, 200, 70, -130, -400, -650]
+*/
+
+// ※ Array Grouping
+
+console.log(movements);
+// → [200, 450, -400, 3000, -650, -130, 70, 1300]
+
+// groupBy
+// 그룹화할 그룹과 콜백 함수를 인수로 받는다.
+const groupedMovements = Object.groupBy(movements, movement =>
+  movement > 0 ? 'deposit' : 'withdrawals'
+);
+console.log(groupedMovements);
+// → {deposit:[200, 450, 3000, 70, 1300], withdrawals: [-400, -650, -130]}
+
+const groupByActivity = Object.groupBy(accounts, account => {
+  const movementCount = account.movements.length;
+
+  if (movementCount >= 8) return 'very active';
+  if (movementCount >= 4) return 'active';
+  if (movementCount >= 1) return 'moderate';
+  return 'inactive';
+});
+console.log(groupByActivity);
+// →
+// {active : [{owner: 'Sarah Smith', ...}],
+// very active : [{owner: 'Jonas Schmedtmann', ...}, {owner: 'Jessica Davis', ...}, {owner: 'Steven Thomas Williams', ...}]
+
+// const groupedAccounts = Object.groupBy(accounts, account => account.type);
+// console.log(groupedAccounts);
+const groupedAccounts = Object.groupBy(accounts, ({ type }) => type);
+console.log(groupedAccounts);
+// →
+// {premium: [{owner: 'Jonas Schmedtmann', ...}, {owner: 'Steven Thomas Williams', ...}],
+// standard: {owner: 'Jessica Davis', ...},
+// basic: [{owner: 'Sarah Smith', ...}]}
