b5fda37af359d0142244c2ac907a041a230bc439
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-01-06 17:32:46 +0900

    📝 chore: review Array methods with examples
    - review Array methods with examples
    - add examples about advanced utilization of the reduce method
    - add examples about prefix and postfix operator
    - review destructuring arrays
    - implements capitalization function
    - follow along with instructor lectures

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -774,6 +774,7 @@ labelBalance.addEventListener('click', function () {
 });
 */
 
+/*
 // ※ Non-Destructive Alternatives: toReversed, toSorted, toSpliced, with
 
 console.log(movements);
@@ -804,3 +805,63 @@ console.log(newMovements);
 // → [200, 2000, -400, 3000, -650, -130, 70, 1300]
 console.log(movements);
 // → [200, 450, -400, 3000, -650, -130, 70, 1300]
+*/
+
+// ※ Array Methods Practice
+
+// 1.
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+
+console.log(bankDepositSum);
+
+// 2.
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+// .filter(mov => mov >= 1000).length;
+
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  // .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+
+console.log(numDeposits1000);
+
+let a = 10;
+console.log(++a); // prefix
+// console.log(a++); // postfix: 값은 변화시키지만, 이전값을 반환한다.
+console.log(a);
+
+// 3.
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+console.log(deposits, withdrawals); // → 25180 -7340
+
+// 4.
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+    .join(' ');
+  return capitalize(titleCase);
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
