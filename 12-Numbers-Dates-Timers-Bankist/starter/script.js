95b42fe4cf7f49729c4a5eb0b003b93571d67b6e
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-01-09 17:35:56 +0900

    ✨ feat: fix decimal point when presenting balance
    - add an example for rounding numbers
    - add examples for sqrt, max, min, random methods
    - add examples of rounding integers (trunc, round, ceil and floor)
    - add an example for rounding decimal (toFixed)
    - drop amount with floor when loan is requested
    - fix decimal point with toFixed when presenting balance

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -252,6 +252,7 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
+/*
 // ※ Converting and Checking Numbers
 console.log(23 === 23.0); // → true
 
@@ -305,3 +306,57 @@ console.log(Number.isFinite(23 / 0)); // → false
 console.log(Number.isInteger(23)); // → true
 console.log(Number.isInteger(23.0)); // → true
 console.log(Number.isInteger(23 / 0)); // → false
+*/
+
+// ※ Math and Rounding
+
+// 1) sqrt (square root)
+console.log(Math.sqrt(25)); // → 5
+console.log(25 ** (1 / 2)); // → 5
+console.log(8 ** (1 / 3)); // → 2
+
+// 2) max, min
+console.log(Math.max(5, 18, 23, 11, 2)); // → 23
+console.log(Math.max(5, 18, '23', 11, 2)); // → 23 (type coercion 적용O)
+console.log(Math.max(5, 18, '23p', 11, 2)); // → NaN (parsing 적용X)
+
+console.log(Math.min(5, 18, 23, 11, 2)); // → 2
+
+// 3) PI
+console.log(Math.PI); // → 3.141592653589793
+console.log(Math.PI * Number.parseFloat('10px') ** 2); // → 314.1592653589793
+
+// 4) random, trunc(내림), floor
+console.log(Math.random()); // 0~1 사이의 랜덤한 값 발생
+console.log(Math.trunc(Math.random() * 6 + 1)); // 1~6의 랜덤한 정수값 발생
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(randomInt(10, 20));
+console.log(randomInt(0, 3));
+
+// 5) Rounding Integers: trunc, round, ceil, floor
+// type coercion이 적용된다.
+console.log(Math.trunc(23.3)); // → 23 // 소수점 절삭
+console.log(Math.round(23.3)); // → 23 // 반올림
+console.log(Math.round(23.9)); // → 24
+
+console.log(Math.ceil(23.3)); // → 24 // 올림
+console.log(Math.ceil(23.9)); // → 24
+
+console.log(Math.floor(23.3)); // → 23 // 내림
+console.log(Math.floor('23.9')); // → 23
+
+// trunc와 floor은 음수와 함께 쓸 때 차이가 생긴다.
+console.log(Math.trunc(-23.3)); // → -23
+console.log(Math.floor(-23.3)); // → -24
+
+// 6) Rounding decimals
+// toFixed 는 문자열을 반환, 괄호 안의 숫자는 소수점 자리수이다.
+console.log((2.7).toFixed(0)); // → 3(문자열)
+console.log((2.7).toFixed(3)); // → 2.700(문자열)
+console.log((2.345).toFixed(2)); // → 2.35(문자열)
+console.log(+(2.345).toFixed(2)); // → 2.35(숫자)
+// String methods 와 유사하게,
+// 원시형(primitive)은 자동으로 Boxing 되어 Math형태가 되므로, 메소드를 바로 쓸 수 있다.
