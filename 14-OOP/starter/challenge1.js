2b3aef0b4dfed53b2b811277ad30cacb5546420a
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-01-25 17:12:31 +0900

    🌈 chore: add challenge #1 of OOP with JS
    - add challenge #1 of Object-Oriented Programming with JavaScript
    - implement JS solution using constructor function and prototype

---
+++ 14-OOP/starter/challenge1.js
@@ -0,0 +1,48 @@
+// Coding Challenge #1
+
+/*
+Your tasks:
+1. Use a constructor function to implement a 'Car'. A car has a 'make' and a
+'speed' property. The 'speed' property is the current speed of the car in km/h
+
+2. Implement an 'accelerate' method that will increase the car's speed by 10,
+and log the new speed to the console
+
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log
+the new speed to the console
+
+4. Create 2 'Car' objects and experiment with calling 'accelerate' and
+'brake' multiple times on each of them
+
+Test data:
+§ Data car 1: 'BMW' going at 120 km/h
+§ Data car 2: 'Mercedes' going at 95 km/h
+*/
+
+// Construtor function
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} going at ${this.speed}km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} going at ${this.speed}km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate(); // → BMW going at 130km/h
+bmw.accelerate(); // → BMW going at 140km/h
+bmw.brake(); // → BMW going at 135km/h
+bmw.accelerate(); // → BMW going at 145km/h
+
+mercedes.brake(); // → Mercedes going at 90km/h
+mercedes.brake(); // → Mercedes going at 85km/h
+mercedes.accelerate(); // → Mercedes going at 95km/h
