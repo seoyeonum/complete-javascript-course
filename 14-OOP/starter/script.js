deb2c367735ddf78ce321064d159a0ee04e96368
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-01-30 13:54:56 +0900

    📝 chore: add examples - inheritance btw classes(2)
    - use ES6 Classes to implement inheritance between classes
    - add examples about extends and super keywords
    - follow along with instructor lectures

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -150,7 +150,7 @@ class PersonCl {
     else alert(`${name} is not a full name!`);
   }
 
-  // _변수명을 return 하기 위한 getter 생성성성
+  // _변수명을 return 하기 위한 getter 생성
   get fullName() {
     return this._fullName;
   }
@@ -247,6 +247,7 @@ sarah.calcAge(); // → 58
 //////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
 
+/*
 // ※ Inheritance Between "Classes": Constructor Functions
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
@@ -288,3 +289,69 @@ console.log(mike instanceof Object); // → true
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor); // → ƒ Student(firstName, birthYear, course)
+*/
+
+// ※ Inheritance Between "Classes": ES6 Classes
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance methods
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    // console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static methods
+  static hey() {
+    console.log('Hey there 🙌🏻');
+    // console.log(this);
+  }
+}
+
+// 단지 extends 키워드로 상속을 구현할 수 있다!
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // Alwats needs to happen first!
+    // PersonCl.call(this, fullName, birthYear) // call method 대신 super 사용
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+
+  // Override calcAGe method
+  calcAge() {
+    console.log(
+      `I'm ${2037 - this.birthYear} years old, but as a student I feel more like ${2037 - this.birthYear + 10}`,
+    );
+  }
+}
+
+// 만약, 추가적인 property가 필요하지 않다면 constructor를 생성하지 않아도 된다...!
+// const martha = new StudentCl('Martha Jones', 2012);
+
+const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+martha.introduce();
+martha.calcAge();
