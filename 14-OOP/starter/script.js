e04de866f1b08537485c3397681681565d1ba285
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-01-31 21:00:45 +0900

    📝 chore: add examples - inheritance btw classes(3)
    - use Object.create to implement inheritance between classes
    - follow along with instructor lectures

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -291,6 +291,7 @@ Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor); // → ƒ Student(firstName, birthYear, course)
 */
 
+/*
 // ※ Inheritance Between "Classes": ES6 Classes
 class PersonCl {
   constructor(fullName, birthYear) {
@@ -355,3 +356,36 @@ class StudentCl extends PersonCl {
 const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
+*/
+
+// ※ Inheritance Between "Classes": Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce(); // → My name is Jay and I study Computer Science
+jay.calcAge(); // → 27
+
+// ※ 실제 현실에서는 ES6 Classes(extend 키워드)를 사용하는 경우가 보편적이다!
