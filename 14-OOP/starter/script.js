493c9a7489904a0eab28b26950a1f9973c49d79e
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-02-01 15:33:42 +0900

    📝 chore: add examples about bankist app
    -  add examples about bankist app
    - (need to be refactored to restrict access to pin from outside)
    - follow along with instructor lectures

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -358,6 +358,7 @@ martha.introduce();
 martha.calcAge();
 */
 
+/*
 // ※ Inheritance Between "Classes": Object.create
 
 const PersonProto = {
@@ -389,3 +390,50 @@ jay.introduce(); // → My name is Jay and I study Computer Science
 jay.calcAge(); // → 27
 
 // ※ 실제 현실에서는 ES6 Classes(extend 키워드)를 사용하는 경우가 보편적이다!
+*/
+
+// ※ Another Class Example
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+acc1.console.log(acc1);
+console.log(acc1.pin);
