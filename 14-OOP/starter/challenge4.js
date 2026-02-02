4e96ec5c67a9ce5046112a6381c0d6e856cbb49b
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-02-02 16:36:41 +0900

    🌈 chore: add challenge #4 of OOP with JS
    - add challenge #4 of Object-Oriented Programming with JavaScript
    - review inheritance between classes using ES6 Classes
    - implement JS solution using private field

---
+++ 14-OOP/starter/challenge4.js
@@ -0,0 +1,86 @@
+// Coding Challenge #4
+/*
+Your tasks:
+1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl'
+child class of the 'CarCl' class
+2. Make the 'charge' property private
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery'
+methods of this class, and also update the 'brake' method in the 'CarCl'
+class. Then experiment with chaining!
+
+Test data:
+§ Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  // Overide methods
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} is going at ${this.speed} km/h, with a charge of ${this.#charge}`,
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian);
+// console.log(rivian.#charge);
+// → Uncaught SyntaxError: Private field '#charge' must be declared in an enclosing class
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
+
+// →
+// Rivian is going at 140 km/h, with a charge of 22
+// Rivian is going at 160 km/h, with a charge of 21
+// Rivian is going at 180 km/h, with a charge of 20
+// Rivian is going at 175 km/h
+// Rivian is going at 195 km/h, with a charge of 49
+
+console.log(rivian.speedUS); // → 121.875
