7a003e1337913a1c95cc46a384019101617146d8
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-02-03 16:25:39 +0900

    📝 chore: add examples about Geolocation API
    - add examples about Geolocation API
    - get current latitude and longitude
    - follow along with instructor lectures

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,18 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// ※ Geolocation API
+if (navigator.geolocation) {
+  // 두 함수는 각각 응답 성공 시와 실패 시의 callback 함수이다.
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`); // → 37.4031765 126.985908
+    },
+    function () {
+      alert('Could not get your position');
+    },
+  );
+}
