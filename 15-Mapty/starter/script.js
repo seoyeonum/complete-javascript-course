c222df8c7772836dee17621d07236be8e5f1c350
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-02-03 23:58:44 +0900

    📝 chore: use a hosted version of Leaflet API
    - use a hosted version of Leaflet library for displaying map
    - follow along with instructor lectures

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -19,6 +19,19 @@ if (navigator.geolocation) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(`https://www.google.pt/maps/@${latitude},${longitude}`); // → 37.4031765 126.985908
+
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13); // 뒤쪽 숫자는 zoom level
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
