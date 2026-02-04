b52eb8944d6b5bac31d7107297dfab806b211570
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-02-04 18:30:13 +0900

    ✨ feat: display Map Marker when the map is clicked
    - display Map Marker on the map when the map is clicked (map.on methods)
    - use property of popup to prevent disappearing
    - use setPopupContent method of Marker to set the popup content

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -21,17 +21,33 @@ if (navigator.geolocation) {
       console.log(`https://www.google.pt/maps/@${latitude},${longitude}`); // → 37.4031765 126.985908
 
       const coords = [latitude, longitude];
+
       const map = L.map('map').setView(coords, 13); // 뒤쪽 숫자는 zoom level
+      // console.log(map);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            }),
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your position');
