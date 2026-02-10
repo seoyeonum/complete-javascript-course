945a9c7897ea4596cd6347043ed11b8f63f972bb
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-02-10 12:23:03 +0900

    📝 comment: add comments about AJAX and APIs
    - add comments about Asynchronous compare with Synchronous
    - add comments about AJAX and API
    - follow along with instructor lectures

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -10,3 +10,34 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+
+// ※ Asynchronous JavaScript, AJAX and APIs
+
+// 1. Synchronous
+// - Most code is synchronous
+// - Synchronous code is executed line by line
+// - Each line of code waits for previos line to finish
+// - Long-running operations block code execution
+
+// 2. Asynchronous
+// - Asynchronous code is executed after a task that runs in the "background" finishes
+// - Asynchronous code is "non-blocking" (e.g. Timer with callback)
+// - Execution doesn't wait for an asynchronous task to finish its work
+// - Callback functions alone do NOT make code asynchronous
+
+// 3. AJAX
+// - Asynchronous JavaScript And XML
+// : Allows us to communicate with remote web servers in an asynchronous way
+// (즉, 웹 서버와 비동기 방식으로 통신할 수 있게 해주는 것)
+// - With AJAX calls, we can request data from web servers dynamically
+
+// 4. API
+// - Application Programming Interface
+// : piece of software that can be used by another piece of software,
+// in order to allow applications to talk to each other
+// - There are be many types of APIs in web development
+// : DOM API, Geolocation API, Own Class API, "Online" API
+// - "Online" API (API)
+// : Application running on a server, that receives requests for data,
+// and sends data back as response
+// - We can build our own web APIs (requires bak-end development, e.g. with node.js) or use 3rd-party APIs.
