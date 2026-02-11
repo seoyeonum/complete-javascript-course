6c81529cbc1115176d0886bc4e7812aa47e144be
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-02-11 20:43:20 +0900

    📝 chore: add example about XMLHttpRequest
    - add example about XMLHttpRequest, old school way of AJAX call
    - use REST Countries API to get datas of each countries
    - follow along with instructor lectures

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -8,36 +8,38 @@ const countriesContainer = document.querySelector('.countries');
 
 // NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
-
+// https://countries-api-836d.onrender.com/countries/
 ///////////////////////////////////////
 
-// ※ Asynchronous JavaScript, AJAX and APIs
+// old school way
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
 
-// 1. Synchronous
-// - Most code is synchronous
-// - Synchronous code is executed line by line
-// - Each line of code waits for previos line to finish
-// - Long-running operations block code execution
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-// 2. Asynchronous
-// - Asynchronous code is executed after a task that runs in the "background" finishes
-// - Asynchronous code is "non-blocking" (e.g. Timer with callback)
-// - Execution doesn't wait for an asynchronous task to finish its work
-// - Callback functions alone do NOT make code asynchronous
+    const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
 
-// 3. AJAX
-// - Asynchronous JavaScript And XML
-// : Allows us to communicate with remote web servers in an asynchronous way
-// (즉, 웹 서버와 비동기 방식으로 통신할 수 있게 해주는 것)
-// - With AJAX calls, we can request data from web servers dynamically
-
-// 4. API
-// - Application Programming Interface
-// : piece of software that can be used by another piece of software,
-// in order to allow applications to talk to each other
-// - There are be many types of APIs in web development
-// : DOM API, Geolocation API, Own Class API, "Online" API
-// - "Online" API (API)
-// : Application running on a server, that receives requests for data,
-// and sends data back as response
-// - We can build our own web APIs (requires bak-end development, e.g. with node.js) or use 3rd-party APIs.
+// 아래 데이터는 비동기 방식으로 값을 가져오기에
+// 실행 때마다 그 순서가 달라질 수 있다.
+getCountryData('korea (republic of');
+getCountryData('canada');
+getCountryData('hungary');
