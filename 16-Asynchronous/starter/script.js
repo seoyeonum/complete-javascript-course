718dbf384467ee0d8f9e4dcf37efee5277a6c9bf
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-02-12 23:29:09 +0900

    📝 chore: add examples about callback hell
    - create execution sequence with nested callback functions
    - add example about callback hell
    - follow along with instructor lectures

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -10,7 +10,7 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 // https://countries-api-836d.onrender.com/countries/
 ///////////////////////////////////////
-
+/*
 // old school way
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
@@ -43,3 +43,76 @@ const getCountryData = function (country) {
 getCountryData('korea (republic of');
 getCountryData('canada');
 getCountryData('hungary');
+*/
+
+// callBack 함수의 실행 sequence를 만들어보자. (feat. Callback Hell)
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country 2
+    // const [neighbour] = data.borders;
+    // if (!neighbour) return;
+    const neighbour = data.borders?.[0]; // use optional chaining
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// callback(1) 함수 내에서 callback 함수(2)를 실행했으므로
+// 실행 순서가 항상 동일하다.
+// getCountryAndNeighbour('hungary');
+getCountryAndNeighbour('usa');
+
+// ※ Callback Hell
+// : 비동기 작업을 순서대로 처리하기 위해 중첩된 콜백이 많은 경우!
+// - 들여쓰기 때문에 코드 앞부분이 삼각형으로 보이는 게 특징!
+// - ES6 이후부터는 "promise" 를 사용해 이를 벗어날 수 있다.
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 second passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
