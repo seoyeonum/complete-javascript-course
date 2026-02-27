01adad2ee18395bb6cb3dc8eaeb5e6d68c020264
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-02-28 00:52:16 +0900

    📝 comment: add comments about modules
    - add comments about what is modules and why it is useful
    - follow along with instructor lectures

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,21 @@
+// ※ An Overview of Modules in JavaScript
+
+// Module
+// : Reusable piece of code that encapsulates implementation details.
+// : Usually a standalone file, but it doesn't have to be.
+
+// Why we use modules?
+// 1) Compose software (SW 구성)
+// : Modules are small building blocks
+//  that we put together to build complex applications.
+// 2) Isolate components (컴포넌트 분리, 독립성)
+// : Modules can be developed in
+//  isolation without thinking about the entire codebase.
+// 3) Abstract code (추상화)
+// : Implement low-level code in modules and
+//  import these abstractions into other modules.
+// 4) Organized code (조직화)
+// : Modules naturally lead to a more organized codebase.
+// 5) Reuse code (코드 재사용)
+// : Modules allow us to easily reuse the same
+//  code, even across multiple projects.
