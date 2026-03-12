44c218cf40cb1573f1fc7ea89308db1b5fb50c68
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-03-12 23:25:16 +0900

    📝 comment: add comments about the MVC architecture
    - add comments about components of any architecture
    - add comments about the Model-View-Controller (MVC) ahitecture
    - follow along with instructor lectures

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -164,3 +164,29 @@ const showRecipe = async function () {
 ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
 // window.addEventListener('hashchange', showRecipe);
 // window.addEventListener('load', showRecipe);
+
+////////////////////////////////////////////////
+
+// ※ The MVC Architecture
+
+// 1. Why worry about architecture?
+// - Structure: the way we organize our code
+// - Maintainability: easily change it in the future
+// - Expandability: easily add new features
+// → The perfect architecture
+
+// 2. Components of any architecture (모든 아키텍쳐가 갖추어야 할 요소들)
+// - Business Logic: code that solves the actual business
+// - State: stores all the data about the application (e.g. Redux, MobX)
+// - HTTP Library: responsible for making and receiving AJAX requests
+// - Application Logic(Router): the implementation of application itself
+// - Presentation Logic(UI Layer): the visible part of the application
+
+// 3. The Model-View-Controller (MVC) Architecture
+// 1) MODEL: Business logic, State, HTTP Library
+//   - interacting with WEB
+// 2) CONTROLLER: Application Logic
+//   - bridge between model and view
+//   - OLNY the controller imports and calls functions
+// 3) VIEW : Presentation Logic
+//   - interacting with USER
