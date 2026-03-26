21fe8b5a1f9a2a8187acc235d6a05aa3e4c53f8c
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-03-26 23:22:17 +0900

    🚧 feat: implement uploading a new recipe (WIP)
    - add addRecipeView.js to show new recipe modal
    - add toggleWindow fn for connecting event listener and this keyword
    - change data array to data object with Object.fromEntries(arr)

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -104,6 +105,10 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -111,6 +116,9 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerupload(controlAddRecipe);
+
+  // Upload the new recip data
 };
 init();
 ////////////////////////////////////////////////
---
+++ 18-forkify/starter/src/js/views/addRecipeView.js
@@ -0,0 +1,46 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
+  }
+
+  toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
+
+  _addHandlerShowWindow() {
+    // this._overlay.classList.toggle('hidden');
+    // this._window.classList.toggle('hidden');
+    // 별도 함수로 정의 후 연결해줘야 this 키워드가 가리키는 값을 조정 가능
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  _addHandlerHideWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  addHandlerupload(handler) {
+    this._parentElement.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const dataArr = [...new FormData(this)];
+      const data = Object.fromEntries(dataArr); // Object으로 전환
+      handler(data);
+    });
+  }
+
+  _generateMarkup() {}
+}
+export default new AddRecipeView();
