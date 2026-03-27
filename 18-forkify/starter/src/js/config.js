5f25fc5b4b32275b6d5dd38523b7e264ef73bfab
Seoyeon Um <seoyeon.ahora@gmail.com>
2026-03-27 17:11:05 +0900

    🚧 feat: implement uploading a new recipe (WIP)
    - receive and separate ingredients when adding new recipes
    - add printing error messages when submitting in the wrong format
    - add sendJSON fn and API key recieved for adding new recipes
    - add printing messages and bookmark when adding a new recipe
    - add loading spinner

--- 18-forkify/starter/src/js/config.js
+++ 18-forkify/starter/src/js/config.js
@@ -1,5 +1,10 @@
 // constant(상수)의 경우, 변수명을 대문자로 작성!
 // 일반적으로 URL은 '/'로 끝난다.
-export const API_URL = ' https://forkify-api.jonas.io/api/v2/recipes/';
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
+
+// ※ 현재 API KEY 가 서버 문제로 작동하지 않으니,
+// <my key from API>에 발급받은 key 넣고 추후 꼭 확인해볼것!!
+export const KEY = '<my key from API>';
